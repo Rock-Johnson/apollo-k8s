e0f5acc278f485d0f86f8907e09c52cdd0768ffd
Rock-Johnson <541767719@qq.com>
2019-07-26 15:04:19 +0800

    chmod

--- docker-pull.sh
+++ docker-pull.sh
@@ -1,25 +1,15 @@
-# create namespace
-kubectl create namespace sre
+#apollo-admin-server
+docker pull registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-admin-server:v1.4.0
+docker tag registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-admin-server:v1.4.0  apollo-admin-server:v1.4.0
+docker rmi registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-admin-server:v1.4.0
 
-# dev-env
-kubectl apply -f apollo-env-dev/service-mysql-for-apollo-dev-env.yaml --record && \
-kubectl apply -f apollo-env-dev/service-apollo-config-server-dev.yaml --record && \
-kubectl apply -f apollo-env-dev/service-apollo-admin-server-dev.yaml --record
+#apollo-config-server:v1.4.0
+docker pull registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-config-server:v1.4.0
+docker tag registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-config-server:v1.4.0 apollo-config-server:v1.4.0
+docker rmi registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-config-server:v1.4.0
 
-# fat-env(test-alpha-env)
-kubectl apply -f apollo-env-test-alpha/service-mysql-for-apollo-test-alpha-env.yaml --record && \
-kubectl apply -f apollo-env-test-alpha/service-apollo-config-server-test-alpha.yaml --record && \
-kubectl apply -f apollo-env-test-alpha/service-apollo-admin-server-test-alpha.yaml --record
 
-# uat-env(test-beta-env)
-kubectl apply -f apollo-env-test-beta/service-mysql-for-apollo-test-beta-env.yaml --record && \
-kubectl apply -f apollo-env-test-beta/service-apollo-config-server-test-beta.yaml --record && \
-kubectl apply -f apollo-env-test-beta/service-apollo-admin-server-test-beta.yaml --record
-
-# prod-env
-kubectl apply -f apollo-env-prod/service-mysql-for-apollo-prod-env.yaml --record && \
-kubectl apply -f apollo-env-prod/service-apollo-config-server-prod.yaml --record && \
-kubectl apply -f apollo-env-prod/service-apollo-admin-server-prod.yaml --record
-
-# portal
-kubectl apply -f service-apollo-portal-server.yaml --record
+#apollo-portal-server:v1.4.0
+docker pull registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-portal-server:v1.4.0
+docker tag registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-portal-server:v1.4.0 apollo-portal-server:v1.4.0 
+docker rmi registry.cn-beijing.aliyuncs.com/zhy_k8s/apollo-portal-server:v1.4.0
